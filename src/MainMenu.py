19bb1d1fb2c6136d93767b073bf14bad7f8298f8
Tomas Aristizabal Gomez <tomasaristizabalgomez@gmail.com>
2025-02-08 22:05:42 -0500

    Menu para el proceso de devoluciones

--- src/MainMenu.py
+++ src/MainMenu.py
@@ -57,7 +57,8 @@ class MainMenu:
                 elif opcion == 2:
                     self.cuentaBancariaDisplay()
                 elif opcion == 3:
-                    #Espacio para realizar devolución
+                    print("\n")
+                    self.returnMenuDisplay()
                     break
                 elif opcion == 4:
                     if len(self.comprador.getCarritoCompras().getListaItems()) == 0:
@@ -290,3 +291,35 @@ class MainMenu:
         return self.inventario
     def setInventario(self, inventario):
         self.inventario = inventario
+
+    def returnMenuDisplay(self):
+        while True:
+            print("===== MENÚ DEVOLUCIÓN =====")
+
+            print("¿Conoce el ID de la factura y del producto a devolver? (S/N): ")
+
+            conoceIdFactura = input().casefold() == "s"
+
+            if (not conoceIdFactura):
+               print("Por favor, consiga la información necesaria en el numeral 6 del MENÚ COMPRADOR.\n")
+               return 
+
+            print("Ingrese el ID de la factura donde se encuentra el producto: ")
+            idFactura = int(input())
+
+            print("Ingrese el ID del producto a devolver: ")
+            idProducto = int(input())
+
+            print("Ingrese la cantidad a devolver: ")
+            cantidadRetornar = int(input())
+
+            resultado = self.comprador.devolverProducto(idFactura, idProducto, cantidadRetornar, self.vendedor) # Proceso de reembolso en si
+            
+            if resultado == "FacturaInvalida":
+                print("La factura ingresada no fue encontrada.\n")
+                break
+            elif resultado == "ProductoInvalido":
+                print("El producto ingresado no cumple con los requisitos para devolución.\n" + "Razones posibles:\n" + "- No se encontró en la factura.\n" + "- No es un producto retornable.\n" + "- La cantidad especificada para devolver no es válida.")
+                break
+            else:
+                print("La devolución se ha procesado correctamente, en sus notificaciones encontrará más información.\n")
