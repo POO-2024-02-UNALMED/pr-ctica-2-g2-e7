bfcaf26452a63c39d69f5ef58450ecda5898b075
santiagobtos <sbarrientosm@unal.edu.co>
2025-02-10 21:07:09 -0500

    mas arreglos del home

--- src/UiMain/Home.py
+++ src/UiMain/Home.py
@@ -1,67 +1,120 @@
 import tkinter as tk
+from PIL import Image, ImageTk
+import os
 
+class App:
+    def __init__(self):  
+        self.contador = 0
+        self.imagen_tk = None
 
+        # Crear ventana principal
+        self.window = tk.Tk()
+        self.window.geometry("800x600")
+        self.window.title("Inicio")
+        # configurar grid es quien configura las columnas y filas de la ventana principal
+        self.configurar_grid()
+        #despues de tener bien organizadas las columnas y filas donde van a estar los frames , pasamos a crear los frames
+        self.crear_frames()
+        
+        self.etiqueta.config(image=self.imagen_tk)
+        self.p4.after(100, self.cargar_imagen_inicial)
+        self.p4.bind("<Enter>", self.imagenes)
+        
+        self.window.mainloop()
+    
+    def configurar_grid(self):
+        margin_x = 10#margenes 
+        margin_y = 10
+        spacing = 20 # espacio entre frame p1 y p2
 
+        self.window.columnconfigure(0, weight=1, minsize=margin_x)
+        self.window.columnconfigure(1, weight=10)
+        self.window.columnconfigure(2, weight=1, minsize=spacing)
+        self.window.columnconfigure(3, weight=15)
+        self.window.columnconfigure(4, weight=1, minsize=margin_x)
+        self.window.rowconfigure(0, weight=1, minsize=margin_y)
+        self.window.rowconfigure(1, weight=30)
+        self.window.rowconfigure(2, weight=1, minsize=margin_y)
+    
+    def crear_frames(self): 
+        #frames principales
+        self.p1 = tk.Frame(self.window)
+        self.p2 = tk.Frame(self.window, bg="red")
 
-window = tk.Tk()
-window.geometry("800x600")
-window.title("Inicio")
+        self.p1.grid(row=1, column=1, sticky="nsew")
+        self.p2.grid(row=1, column=3, sticky="nsew")
 
+        self.p1.columnconfigure(0, weight=1)
+        self.p1.rowconfigure(1, weight=1)
+        self.p1.rowconfigure(2, weight=1)
 
-margin_x = 10 # con esto nos aseguramos de las margenes en los bordes, por este lado, de las margenes en los bordes de x
-margin_y = 10 #por este lado en las de y 
-spacing = 20  # espacio entre los frames
+        self.p3 = tk.Frame(self.p1)
+        self.p3.grid(row=1, column=0, sticky="nsew", padx=10, pady=10)
 
-# Configurar el grid para que crezca 
-#vamos a configurar las columnas , 5 columnas, dos donde van los frames, y tres espaciadoras, entre margenes y entre los frames
+        self.p4 = tk.Frame(self.p1)
+        self.p4.grid(row=2, column=0, sticky="nsew", padx=10, pady=10)
+        self.p4.grid_propagate(False)
 
-window.columnconfigure(0, weight=1, minsize=margin_x)
-window.columnconfigure(1, weight=10)  # Columna para el primer frame # el weight es diferente ya que es mejor tener mas espacio para los frames y poder tenerlos con buena visibilidad
-window.columnconfigure(2, weight=1, minsize=spacing)  # el weigth es el que se asegura del crecimiento de la ventana, hice mas grandes los de los frames para que crezcan como con mejor vista y pues proporcionales
-window.columnconfigure(3, weight=20)  # Columna para el segundo frame
-window.columnconfigure(4, weight=1, minsize=margin_x)
+        self.p2.rowconfigure(1, weight=1)
+        self.p2.rowconfigure(2, weight=1)
+        self.p2.columnconfigure(0, weight=1)
+        
+        self.p5 = tk.Frame(self.p2, bg="purple")
+        self.p5.grid(row=1, column=0, sticky="nsew", padx=10, pady=10)
+        
+        self.p6 = tk.Frame(self.p2, bg="orange")
+        self.p6.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
+        
+        self.p3.columnconfigure(0, weight=1)
+        self.p3.rowconfigure(1, weight=1)
+        #el label que da la bienvenida
+        texto_bienvenida = tk.Label(
+            self.p3, 
+           text="Bienvenido a nuestra tienda virtual, \n\nesperamos que nuestro proyecto de \n\nprogramación orientada a objetos \n\nsatisfaga todas tus necesidades.",
+            wraplength=350,
+            font=("Arial", 11, "bold"), 
+            justify="left"
+        )
+        texto_bienvenida.grid(column=0, row=1, sticky="nsew", padx=3, pady=3)
 
-window.rowconfigure(0, weight=1, minsize=margin_y)
-window.rowconfigure(1, weight=30)  # Fila para los frames
-window.rowconfigure(2, weight=1, minsize=margin_y)
+        self.p4.columnconfigure(0, weight=1)
+        self.p4.rowconfigure(0, weight=3)
+        self.p4.rowconfigure(1, weight=1)
+        # el boton que nos da el acceso al sistema 
+        self.boton_accesosistema = tk.Button(self.p4, text="Ingresar al sistema")
+        self.boton_accesosistema.grid(column=0, row=1, sticky="nsew", padx=2, pady=2)
+        #el label que va a contener las imagenes asociadas al sistema
+        self.etiqueta = tk.Label(self.p4)
+        self.etiqueta.grid(column=0, row=0, sticky="nsew")
+    
+    def cargar_imagen_inicial(self):
+        directorio_actual = os.path.dirname(os.path.abspath(__file__))
+        ruta = os.path.join(directorio_actual, "imagenes", "imagen1.jpg")
+        imagen = Image.open(ruta)
 
-# Crear los frames
-p1 = tk.Frame(window, bg="yellow")
-p2 = tk.Frame(window, bg="red")
+        ancho = self.p4.winfo_width()
+        alto = self.p4.winfo_height()
+        imagen = imagen.resize((ancho, alto))
+        self.imagen_tk = ImageTk.PhotoImage(imagen)
+        self.etiqueta.config(image=self.imagen_tk)
+        self.etiqueta.imagen = self.imagen_tk
+    
+    def imagenes(self, evento):
+        self.window.update_idletasks()
+        ancho = self.p4.winfo_width()
+        alto = self.p4.winfo_height()
 
-p1.grid(row=1, column=1, sticky="nsew") #el nsew se asegura de que crezca en todas las direcciones 
-p2.grid(row=1, column=3, sticky="nsew")
+        directorio_actual = os.path.dirname(os.path.abspath(__file__))
+        rutas = ["imagen1.jpg", "imagen2.jpg", "imagen3.jpg", "imagen4.jpg", "imagen5.jpg"]
+        ruta = os.path.join(directorio_actual, "imagenes", rutas[self.contador])
+        
+        imagen = Image.open(ruta)
+        imagen = imagen.resize((ancho, alto))
+        self.imagen_tk = ImageTk.PhotoImage(imagen)
+        self.etiqueta.config(image=self.imagen_tk)
+        self.etiqueta.imagen = self.imagen_tk
 
-p1.columnconfigure(0, weight=1, minsize=margin_x)
+        self.contador = (self.contador + 1) %5
 
-
-p1.rowconfigure(1, weight=1)
-p1.rowconfigure(2,weight=1)
-p3= tk.Frame(p1,bg="blue")
-p3.grid(row=1, column=0,sticky="nsew",padx=10,pady=10)
-
-
-#aqui vamos a crear el frame que deberia llevar el boton que nos lleva a la ventana principal
-p4=tk.Frame(p1,bg="green")
-p4.grid(row=2,column=0,sticky="nsew",padx=10,pady=10 ) #padx y pady hacen referencia a las margenes 
-#vamos a crear los frame p5 y p6
-p5=tk.Frame(p2, bg="purple")
-p2.rowconfigure(1,weight=1)
-p2.rowconfigure(2,weight=1)
-p2.columnconfigure(0,weight=1,minsize=margin_x)
-p5.grid(row=1,column=0,sticky="nsew",padx=10,pady=10)
-p6=tk.Frame(p2,bg="orange")
-p6.grid(column=0,row=2,padx=10,pady=10,sticky="nsew")
-
-#ahora vamos a proseguir con el marco de bienvenida
-p3.columnconfigure(0,weight=1,minsize=1)
-p3.rowconfigure(1,weight=1)
-
-texto_bienvenida = tk.Label(
-    p3, 
-    text="Bienvenido a nuestra tienda virtual, esperamos que nuestro proyecto de programación orientada a objetos satisfaga todas tus necesidades.",
-    wraplength=150
-)
-
-texto_bienvenida.grid(column=0, row=1, sticky="nsew",padx=3,pady=3)
-window.mainloop()
+if __name__ == "__main__":
+    App()
